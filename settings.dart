9625d665eb7fdf07e6cac4cd6440a196a5bbe9db
Leo Sandler <leo.sandler@queensu.ca>
2021-03-15 13:44:19 -0400

    Commented out settings.dart as it doesn't work. So it doesn't mess with other files

--- settings.dart
+++ settings.dart
@@ -1,11 +1,13 @@
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
+//
+// class SecondRoute extends StatelessWidget{
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(appBar: AppBar(
+//       title: Text('Settings'),
+//     ),
+//       body: Center(child: ElevatedButton(onPressed: (){}, child: new Text('Go back!'),),),);
+//   }
+// }
 
-class SecondRoute extends StatelessWidget{
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(appBar: AppBar(
-      title: Text('Settings'),
-    ),
-      body: Center(child: ElevatedButton(onPressed: (){}, child: new Text('Go back!'),),),);
-  }
-}
+// TODO: edit this code so that it works. currently showing as an error
