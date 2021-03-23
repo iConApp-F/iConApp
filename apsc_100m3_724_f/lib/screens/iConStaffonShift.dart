778464e2f72e3d502ae567b0143295b7c4601794
lambertjacob <75557870+lambertjacob@users.noreply.github.com>
2021-03-22 21:55:30 -0400

    added appbar to off-screens of the team and help pages

--- apsc_100m3_724_f/lib/screens/iConStaffonShift.dart
+++ apsc_100m3_724_f/lib/screens/iConStaffonShift.dart
@@ -108,9 +108,28 @@ class iConOnShiftTwoPage extends StatelessWidget{
   @override
   Widget build (BuildContext context){
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(OnShiftnames[k]),
-      ),
+      appBar: AppBar(title: Text('iCons'),
+          actions: <Widget>[IconButton(icon: Image.asset('lib/images/gear.png'), onPressed:(){Navigator.pushNamed(context, '/settings');
+            // or use: context, MaterialPageRoute(builder: (context) => SecondRoute()),
+          })],
+          leading: IconButton(
+            icon: Image.asset('lib/images/app_swirl.png'), onPressed: (){},
+          ),
+          flexibleSpace: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.7),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Palette.iBrown, Palette.iGreen, Palette.iBlue]),))),
+      //END of AppBar
       //run the code that sets up what the page looks like
       body: IndividualOnShiftBodyWidget(),
     );
