240e83fd237957affc639a29850464d82549e8c7
lambertjacob <75557870+lambertjacob@users.noreply.github.com>
2021-03-22 20:38:43 -0400

    App bar on team and help screens

--- apsc_100m3_724_f/lib/screens/iConStaffonShift.dart
+++ apsc_100m3_724_f/lib/screens/iConStaffonShift.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'hours.dart';
 
 void main() {
   runApp(MyApp());
@@ -43,7 +44,30 @@ class onShiftBodyWidget extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     //make a list view
-    return new Scaffold(body: ListView(
+    return new Scaffold(appBar: AppBar(title: Text('iCons'),
+        actions: <Widget>[IconButton(icon: Image.asset('lib/images/gear.png'),
+            onPressed: () {print('Go to Equipment'); Navigator.pushNamed(context, '/settings');
+              // or use: context, MaterialPageRoute(builder: (context) => SecondRoute()),
+            })],
+        leading: IconButton(
+          icon: Image.asset('lib/images/app_swirl.png'), onPressed: (){},
+        ),
+        flexibleSpace: Container(
+            decoration: BoxDecoration(
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.grey.withOpacity(0.7),
+                  spreadRadius: 5,
+                  blurRadius: 5,
+                  offset: Offset(0, 3), // changes position of shadow
+                ),
+              ],
+              gradient: LinearGradient(
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  colors: [Palette.iBrown, Palette.iGreen, Palette.iBlue]),))),
+
+        body: ListView(
         padding: const EdgeInsets.all(8),
         //generate the list of the iCons on shift
         children: new List.generate(
--- apsc_100m3_724_f/lib/screens/iConsStaff.dart
+++ apsc_100m3_724_f/lib/screens/iConsStaff.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'hours.dart';
 
 void main() {
   runApp(MyApp());
@@ -42,7 +43,29 @@ var imagesize = 40.0;
 class Staff extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
-      return new Scaffold(
+      return new Scaffold(appBar: AppBar(title: Text('iCons'),
+          actions: <Widget>[IconButton(icon: Image.asset('lib/images/gear.png'),
+              onPressed: () {print('Go to Equipment'); Navigator.pushNamed(context, '/settings');
+                // or use: context, MaterialPageRoute(builder: (context) => SecondRoute()),
+              })],
+          leading: IconButton(
+            icon: Image.asset('lib/images/app_swirl.png'), onPressed: (){},
+          ),
+          flexibleSpace: Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.7),
+                    spreadRadius: 5,
+                    blurRadius: 5,
+                    offset: Offset(0, 3), // changes position of shadow
+                  ),
+                ],
+                gradient: LinearGradient(
+                    begin: Alignment.centerLeft,
+                    end: Alignment.centerRight,
+                    colors: [Palette.iBrown, Palette.iGreen, Palette.iBlue]),))),
+
         body: ListView(
             padding: const EdgeInsets.all(8),
             //used to create the widgets of each staff member
