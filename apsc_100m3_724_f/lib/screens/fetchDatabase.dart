7f1f6b42e92f0b2b226f9b2fe00759c5351fb239
Leo Sandler <leo.sandler@queensu.ca>
2021-03-31 14:31:34 -0400

    Gettig rid of some unused libraries

--- apsc_100m3_724_f/lib/screens/fetchDatabase.dart
+++ apsc_100m3_724_f/lib/screens/fetchDatabase.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 import 'dart:io';
 import 'MyGlobals.dart' as gl;
 
--- apsc_100m3_724_f/lib/screens/inventory.dart
+++ apsc_100m3_724_f/lib/screens/inventory.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 import 'dart:convert';
-import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
 import 'fetchDatabase.dart'; // For updating states
 import 'MyGlobals.dart' as gl;
 
